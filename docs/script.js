ccdf0bdc89761a51a008f49f18be66ae13fbde30
Uguisu32J <uguisu32j@gmail.com>
2022-03-30 11:17:42 +0900

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,32 +1,35 @@
 const download = () => {
     const reader = new FileReader();
-    const icon = new Image();
-    const defaultIconSize = 624;
-    const size = 1024;
+    reader.onloadend = () => {
+        generate(reader.result);
+    }
+    reader.readAsDataURL(document.getElementById('file').files[0]);
+}
+
+async function generate(iconSource) {
     const mask = new Image();
     mask.src = 'mask.png';
-    const file = document.getElementById('file').files[0];
-    reader.onloadend = () => {
-        icon.onload = () => {
-            const canvas = document.createElement('canvas');
-            canvas.width = size;
-            canvas.height = size;
-            const scale = document.getElementById('scale').value;
-            const context = canvas.getContext('2d');
-            context.drawImage(mask, 0, 0, size, size);
-            const iconPosition = (size - scale * defaultIconSize) / 2;
-            const iconSize = scale * defaultIconSize;
-            context.drawImage(icon, iconPosition, iconPosition, iconSize, iconSize);
-            const url = canvas.toDataURL('image/png');
-            const a = document.createElement('a');
-            document.body.appendChild(a);
-            a.download = file.name;
-            a.href = url;
-            a.click();
-            a.remove();
-            URL.revokeObjectURL(url);
-        }
-        icon.src = reader.result;
-    }
-    reader.readAsDataURL(file);
+    const icon = new Image();
+    icon.src = iconSource;
+    await mask.decode();
+    await icon.decode();
+    const size = 1024;
+    const canvas = document.createElement('canvas');
+    canvas.width = size;
+    canvas.height = size;
+    const context = canvas.getContext('2d');
+    context.drawImage(mask, 0, 0, size, size);
+    const scale = document.getElementById('scale').value;
+    const defaultIconSize = 824;
+    const iconPosition = (size - scale * defaultIconSize) / 2;
+    const iconSize = scale * defaultIconSize;
+    context.drawImage(icon, iconPosition, iconPosition, iconSize, iconSize);
+    const url = canvas.toDataURL('image/png');
+    const a = document.createElement('a');
+    document.body.appendChild(a);
+    a.download = '';
+    a.href = url;
+    a.click();
+    a.remove();
+    URL.revokeObjectURL(url);
 }
